1b90417326c14174d23f130c44b15e9a51dc160a
Big Homie KG <retop56@gmail.com>
2024-11-11 03:34:02 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
-import socket  # noqa: F401
+# import socket  # noqa: F401
+import asyncio
 
 
-def main():
+def connection_handler(reader, writer):
+    return
+
+
+async def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # server_socket.accept()  # wait for client
+    server_socket = await asyncio.start_server(connection_handler, "localhost", 6379)
+    async with server_socket:
+        await server_socket.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
